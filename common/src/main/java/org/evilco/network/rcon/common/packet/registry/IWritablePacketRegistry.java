1fa1ab255505b8afeb68fdec37109bf6a73dec2c
Johannes Donath <johannesd@evil-co.com>
2014-09-24 18:27:51 +0200

    Added definition for writable packet registry.

---
+++ common/src/main/java/org/evilco/network/rcon/common/packet/registry/IWritablePacketRegistry.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2014 Johannes Donath <johannesd@evil-co.com>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.evilco.network.rcon.common.packet.registry;
+
+import org.evilco.network.rcon.common.packet.IPacket;
+
+/**
+ * @author Johannes Donath <johannesd@evil-co.com>
+ * @copyright Copyright (C) 2014 Evil-Co <http://www.evil-co.com>
+ */
+public interface IWritablePacketRegistry extends IPacketRegistry {
+
+	/**
+	 * Registers a new packet.
+	 * @param packetID The packetID.
+	 * @param packetClass The packet class.
+	 */
+	public void registerPacket (int packetID, Class<? extends IPacket> packetClass);
+}
