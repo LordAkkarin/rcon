75968c10e9357b6c74d94d86eff70c087bbec380
Johannes Donath <johannesd@evil-co.com>
2014-09-24 18:33:43 +0200

    Defined a few packet related events.

---
+++ common/src/main/java/org/evilco/network/rcon/common/event/packet/PacketSendEvent.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2014 Johannes Donath <johannesd@evil-co.com>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.evilco.network.rcon.common.event.packet;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+import org.evilco.network.rcon.common.event.AbstractCancellableEvent;
+import org.evilco.network.rcon.common.packet.PacketWrapper;
+
+/**
+ * @author Johannes Donath <johannesd@evil-co.com>
+ * @copyright Copyright (C) 2014 Evil-Co <http://www.evil-co.com>
+ */
+@AllArgsConstructor
+public class PacketSendEvent extends AbstractCancellableEvent {
+
+	/**
+	 * Stores the packet.
+	 */
+	@Getter
+	@Setter
+	@NonNull
+	private PacketWrapper packet;
+}
